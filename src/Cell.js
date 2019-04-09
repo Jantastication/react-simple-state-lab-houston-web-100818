71f47d1abda6476897bf20cc33a103177d6ae8a7
github username <github email address>
2019-04-09 16:59:02 -0500

    review done

--- src/Cell.js
+++ src/Cell.js
@@ -8,9 +8,9 @@ export default class Cell extends Component {
     };
   }
 
-  updateColor = () => {
-    const newColor = "#333";
-    this.setState({ color: newColor });
+  updateCol = () => {
+    const newCol = "#333";
+    this.setState({ color: newCol });
   };
 
   render() {
@@ -18,9 +18,37 @@ export default class Cell extends Component {
       <div
         className="cell"
         style={{ backgroundColor: this.state.color }}
-        onClick={this.updateColor}
+        onClick={this.updateCol}
       />
+
+      // </div>
     );
   }
-  return;
 }
+
+// import React, { Component } from "react";
+
+// export default class Cell extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       color: this.props.value
+//     };
+//   }
+
+//   updateColor = () => {
+//     const newColor = "#333";
+//     this.setState({ color: newColor });
+//   };
+
+//   render() {
+//     return (
+//       <div
+//         className="cell"
+//         style={{ backgroundColor: this.state.color }}
+//         onClick={this.updateColor}
+//       />
+//     );
+//   }
+//   return;
+// }
